cbbc2c25ae2f5d4eb29750ccb5cc2b4d783c3f3b
Arnaud Foraison <arnaud.foraison@gmail.com>
2018-01-22 00:39:58 +0100

    chore: add shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './shared/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
