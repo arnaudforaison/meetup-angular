a182c8751a973f595daa41f151c8589eb1dfdfd8
Arnaud Foraison <arnaud.foraison@gmail.com>
2018-01-23 19:24:06 +0100

    feat: prepare before call

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,11 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  meetup: {
+    apiKey: '847242b29645f52c5c68566d17566b'
+  },
+  weather: {
+    apiKey: '214cc40c26523dcdcdf43a954c23981a'
+  }
 };
